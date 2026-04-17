cb6cc16017f4507a4cd77e70e0fc2a6169bd30fd
AbuuRasel <jraselahmad@gmail.com>
2026-04-17 19:49:05 +0600

    fix the footer bug. it's shows 2time at the same time one under another

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Banner from './Components/Banner/Banner'
 import Footer from './Components/Footer/Footer'
+import Friends from './Components/Friends/Friend'
+import FriendDetail from './Components/Friends/FriendDetail' 
 
-function App() {
-
+function Home() {
   return (
     <>
-    <Navbar></Navbar>
-    <Banner></Banner>
-    <Footer></Footer>
+      <Banner />
+      <Friends />
     </>
+  )
+}
+
+function FriendsPage() {
+  return (
+    <>
+      <Friends />
+    </>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar /> 
+      <main className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/friends" element={<FriendsPage />} />
+          <Route path="/friend/:id" element={<FriendDetail />} /> 
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
